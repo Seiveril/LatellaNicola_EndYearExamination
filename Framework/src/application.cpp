90125e3e0e4cfcd771af6d724f10486505452a6d
Nicola <nicola.latella02@gmail.com>
2022-06-20 18:23:25 +0200

    Fixing repo

--- Framework/src/application.cpp
+++
@@ -1,8 +0,0 @@
-#include "application.h"
-#include <iostream>
-#include <SFML/Graphics.hpp>
-
-void Application::createWindow(const float windowWidth, const float windowHeight, const char* title)
-{
-	sf::RenderWindow(sf::VideoMode(windowWidth, windowHeight), title);
-}
