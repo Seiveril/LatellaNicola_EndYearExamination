4b8d13a2dd9c214e230719e6d3919e8dbb3fe4d0
Nicola <nicola.latella02@gmail.com>
2022-06-20 18:23:47 +0200

    Fixing repo

---
+++ Framework/src/application.h
@@ -0,0 +1,18 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+class Application
+{
+public:
+
+	void createWindow(float windowWidth, float windowHeight, const char* windowTitle);
+
+private:
+
+};
+
+#endif
